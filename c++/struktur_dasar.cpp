b9a405e2f1235758d4c11dbf5a725adf81942728
Reyhan Dany A.H. Mohammad <121990861+ree2507@users.noreply.github.com>
2023-12-26 17:09:58 +0800

    Add files via upload

---
+++ c++/struktur_dasar.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // preprocessor directives
+
+using namespace std; // deklarasi variabel global
+int main() // fungsi utama
+{
+    cout << "hello world!" << endl;
+    cout << "rey" << endl << endl << endl;
+    cout << "kelas TI B";
+    return 0;
+}
